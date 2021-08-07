076f32ccb60750da493772e9acf7421e50602c04
Allen.Y <56961826+Alleny244@users.noreply.github.com>
2021-08-07 21:23:49 +0530

    Added first loading screen

---
+++ lib/pages/youTube.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Youtube extends StatefulWidget {
+  @override
+  _YoutubeState createState() => _YoutubeState();
+}
+
+class _YoutubeState extends State<Youtube> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Text("Youtube"),
+      ),
+    );
+  }
+}
